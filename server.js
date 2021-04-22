d1feeb49e4a0da9ee996b2084690be159e25aee6
Marie <marie.marcos25@gmail.com>
2021-04-21 22:36:48 -0700

    movie and weather cache hit-miss, need to fix front end

--- server.js
+++ server.js
@@ -5,14 +5,26 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const weather = require('./modules/weather.js');
+const movies = require('./modules/movies.js');
 
 app.use(cors());
 app.get('/weather', weatherHandler);
+app.get('/movies', movieHandler);
 
 
 function weatherHandler(request, response) {
   const { lat, lon } = request.query;
   weather(lat, lon)
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send('Sorry. Something went wrong!');
+    });
+}
+
+function movieHandler(request, response) {
+  const { city } = request.query;
+  movies(city)
     .then(summaries => response.send(summaries))
     .catch((error) => {
       console.error(error);
