73d698ecbf4f118c75b5b30c449d28823f970452
Marie <marie.marcos25@gmail.com>
2021-04-22 13:53:13 -0700

    movie/weather cache hit-miss with time validation

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const weather = require('./modules/weather.js');
-const movies = require('./modules/movies.js');
+const getMovies = require('./modules/movies.js');
 
 app.use(cors());
 app.get('/weather', weatherHandler);
-app.get('/movies', movieHandler);
+app.get('/movies', getMovies);
 
 
 function weatherHandler(request, response) {
@@ -22,15 +22,15 @@ function weatherHandler(request, response) {
     });
 }
 
-function movieHandler(request, response) {
-  const { city } = request.query;
-  movies(city)
-    .then(summaries => response.send(summaries))
-    .catch((error) => {
-      console.error(error);
-      response.status(200).send('Sorry. Something went wrong!');
-    });
-}
+// function movieHandler(request, response) {
+//   const { city } = request.query;
+//   movies(city)
+//     .then(summaries => response.send(summaries))
+//     .catch((error) => {
+//       console.error(error);
+//       response.status(200).send('Sorry. Something went wrong!');
+//     });
+// }
 
 app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
 
